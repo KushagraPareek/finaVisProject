78fa4074485e848af1fcd0b245f47e8610bbd067
ng0035 <nikitagupta0809@gmail.com>
2020-05-16 13:30:10 +0530

    added changes

--- project.py
+++ project.py
@@ -5,17 +5,18 @@ import json
 app = Flask(__name__)
 
 
+cases = pd.read_csv('static/data/testing_data.csv')
+cases.columns = ['test_month','county','positives','cum_positives','total_tests','cum_tests']
+
 @app.route('/')
 def hello_world():
     clean_data()
     return render_template("index.html")
 
+
 def clean_data():
     global cases_df
-    
-    
-    cases_data = pd.read_csv('static/data/testing_data.csv')
-    cases_data.columns = ['test_month','county','positives','cum_positives','total_tests','cum_tests']
+    cases_data = cases
     cases_data.drop(['cum_positives','cum_tests'],axis=1,inplace=True)
     cases_data['test_month'] = pd.to_datetime(cases_data['test_month'].str.strip(),format='%m/%d/%Y').dt.month_name()
     cases_df = cases_data.groupby(['test_month','county'],as_index = False).agg({'positives': 'sum', 'total_tests': 'sum'})
@@ -29,5 +30,4 @@ def colorpleth():
         county_data = list(zip(cases_df['county'].to_list(),
                               cases_df['positives'].to_list(),cases_df['total_tests'].to_list()))
         return json.dumps({'county_data':county_data})     
-
 
